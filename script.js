13e23ae0a64b72504c0d3b9e024c92e807a406e6
Raphael Macedo <raphaa.macedo@hotmail.com>
2021-04-21 10:35:08 -0300

    aprendendo condicionais

---
+++ script.js
@@ -0,0 +1,20 @@
+var idade = 17;
+
+if (idade >= 18 && idade <= 70)
+   {console.log("pode");
+   console.log("Qual o seu pedido");
+}
+else if (idade <=15)
+    {console.log("Não pode");
+    console.log("E-mail enviado para o responsável");
+    }
+else if (idade >=70)
+    {console.log("Não pode");
+    console.log("Lei Municipal");
+    }
+
+else{
+   console.log("não pode");
+   console.log("Volte futuramente");
+}
+
